1ee1f60f6d2c83af5d13e47ffa83b416effe09e4
Zicrus2311 <41306555+Zicrus2311@users.noreply.github.com>
2021-12-08 12:51:37 +0100

    Whack a mole

--- src/games/whackAMole.cpp
+++ src/games/whackAMole.cpp
@@ -4,6 +4,38 @@
 
 #include "games.h"
 
+bool whackAMole::pressedLastFrame = false;
+int whackAMole::curLed = 0;
+int whackAMole::lastLed = 0;
+int whackAMole::lastPressTime = 0;
+bool whackAMole::travelling = false;
+
 void whackAMole::gameLoop() {
-    games::fillStrip(CRGB(255, 0, 255));
+    if(*games::stopDown && !pressedLastFrame && !travelling) {
+        lastPressTime = millis();
+        lastLed = curLed;
+        curLed = random(0, games::numLeds);
+        travelling = true;
+    }
+    pressedLastFrame = *games::stopDown;
+
+    if (travelling) {
+        int dir = curLed > lastLed ? 1 : -1;
+        long travelTime = millis() - lastPressTime;
+        int tempLed = lastLed + travelTime * dir * games::numLeds / 1000;
+        tempLed = constrain(tempLed, 0, games::numLeds - 1);
+
+        if(abs(tempLed - lastLed) >= abs(curLed - lastLed)) {
+            travelling = false;
+        }
+        else {
+            games::fillStrip(CRGB(0, 0, 0));
+            games::leds[tempLed] = CRGB(255, 0, 0);
+        }
+    }
+    if  (!travelling) {
+        games::fillStrip(CRGB(0, 0, 0));
+        games::leds[curLed] = CRGB(255, 255, 255);
+    }
+    
 }
--- src/games/whackAMole.h
+++ src/games/whackAMole.h
@@ -1,6 +1,13 @@
 #pragma once
 
 class whackAMole {
+private:
+    static bool pressedLastFrame;
+    static int curLed;
+    static int lastLed;
+    static int lastPressTime;
+    static bool travelling;
+
 public:
     static void gameLoop();
 };
